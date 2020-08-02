3d56bd01b88a16f406868fb4a69b19357eb1fdd4
sambarannnn <63100608+sambarannnn@users.noreply.github.com>
2020-08-02 18:03:21 +0530

    Add files via upload

---
+++ q1.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int i,n,a[100];
+
+
+   printf("number of elements to store in the array :");
+   scanf("%d",&n);
+
+   printf("Input %d number of elements in the array :\n",n);
+   for(i=0;i<n;i++)
+      {
+      printf("element - %d : ",i);
+      scanf("%d",&a[i]);
+      }
+
+   printf("\nvalues oF the array are : \n");
+   for(i=0;i<n;i++)
+     {
+       printf("% 5d",a[i]);
+     }
+
+   printf("\n values of array in reverse are :\n");
+   for(i=n-1;i>=0;i--)
+      {
+       printf("% 5d",a[i]);
+      }
+   printf("\n\n");
+}
