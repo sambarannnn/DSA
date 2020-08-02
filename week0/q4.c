6c55850d33a2670c8877abf7c262cc1a1aeb8116
sambarannnn <63100608+sambarannnn@users.noreply.github.com>
2020-08-03 01:52:05 +0530

    Add files via upload

---
+++ week0/q4.c
@@ -0,0 +1,33 @@
+// Count the number of duplicate elements in an array.
+#include <stdio.h>
+
+int main()
+{
+    int arr[100];
+    int i, j, n, count = 0;
+
+    printf("Enter size of the array : ");
+    scanf("%d", &n);
+
+    printf("Enter elements in array : ");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i=0; i<n; i++)
+    {
+        for(j=i+1; j<n; j++)
+        {
+            if(arr[i] == arr[j])
+            {
+                count++;
+                break;
+            }
+        }
+    }
+
+    printf("\nTotal number of duplicate elements found in array = %d", count);
+
+    return 0;
+}
