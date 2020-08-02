311cf7cba5dd643cc208beaf37b4d58170339d32
sambarannnn <63100608+sambarannnn@users.noreply.github.com>
2020-08-03 01:49:15 +0530

    Add files via upload

---
+++ week0/q2.c
@@ -0,0 +1,28 @@
+// Read n no. of values into an array and display them in reverse order.
+#include <stdio.h>
+
+int main()
+{
+    int i,n,a[100] ;
+
+
+   printf("number of elements to store in the array :");
+   scanf("%d",&n);
+
+   printf("Input %d number of elements in the array :\n",n);
+   for(i=0;i<n;i++)
+      {
+      printf("element - %d : ",i);
+      scanf("%d",&a[i]);
+
+      }
+    int sum = 0;
+    for(i=0; i<n; i++)
+        {
+
+            sum+=a[i];
+        }
+         printf("sum of array is : %d",sum);
+
+
+}
